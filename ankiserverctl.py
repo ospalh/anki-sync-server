74b6cb5786f8ea91ace95a49612a78d681047917
ospalh <ospalh@gmail.com>
2013-12-13 14:48:19 +0100

    Use argparse.

--- ankiserverctl.py
+++ ankiserverctl.py
@@ -7,16 +7,17 @@ Controller for the Anki Sync Server.
 
 from __future__ import print_function
 
-import os
-import sys
-import signal
-import subprocess
+import argparse
 import binascii
 import getpass
 import hashlib
+import os
+import signal
 import sqlite3
+import subprocess
+import sys
 
-SERVERCONFIG = "production.ini"
+# SERVERCONFIG = "production.ini"
 AUTHDBPATH = "auth.db"
 PIDPATH = "/tmp/ankiserver.pid"
 COLLECTIONPATH = "collections/"
@@ -37,27 +38,26 @@ Commands:
 """.format(sys.argv[0]))
 
 
-def startsrv(configpath):
-    if not configpath:
-        configpath = SERVERCONFIG
+def startsrv(args):
+    configfile = args.configfile
 
     # We change to the directory containing the config file
     # so that all the paths will be relative to it.
-    configdir = os.path.dirname(configpath)
+    configdir = os.path.dirname(configfile)
     if configdir != '':
         os.chdir(configdir)
-    configpath = os.path.basename(configpath)
+    configfile = os.path.basename(configfile)
 
     devnull = open(os.devnull, "w")
 
     pid = subprocess.Popen(
-        ["paster", "serve", configpath], stdout=devnull, stderr=devnull).pid
+        ["paster", "serve", configfile], stdout=devnull, stderr=devnull).pid
 
     with open(PIDPATH, "w") as pidfile:
         pidfile.write(str(pid))
 
 
-def stopsrv():
+def stopsrv(args):
     if os.path.isfile(PIDPATH):
         try:
             with open(PIDPATH) as pidfile:
@@ -66,13 +66,15 @@ def stopsrv():
                 os.kill(pid, signal.SIGKILL)
                 os.remove(PIDPATH)
         except Exception, error:
-            print >>sys.stderr, \
-                sys.argv[0]+": Failed to stop server: "+error.message
+            print("{}: Failed to stop server: {}".format(
+                    sys.argv[0], error.message), file=sys.stderr)
     else:
-        print >>sys.stderr, sys.argv[0]+": The server is not running"
+        print("{}: The server is not running".format(sys.argv[0]),
+              file=sys.stderr)
 
 
-def adduser(username):
+def adduser(args):
+    username = args.username
     if username:
         print("Enter password for {}: ".format(username))
 
@@ -97,7 +99,8 @@ def adduser(username):
         usage()
 
 
-def deluser(username):
+def deluser(args):
+    username = args.username
     if username and os.path.isfile(AUTHDBPATH):
             conn = sqlite3.connect(AUTHDBPATH)
             cursor = conn.cursor()
@@ -109,11 +112,11 @@ def deluser(username):
     elif not username:
         usage()
     else:
-        print ("{}: Database file does not exist".format(sys.argv[0]),
+        print("{}: Database file does not exist".format(sys.argv[0]),
                file=sys.stderr)
 
 
-def lsuser():
+def lsuser(args):
     conn = sqlite3.connect(AUTHDBPATH)
     cursor = conn.cursor()
 
@@ -127,7 +130,8 @@ def lsuser():
     conn.close()
 
 
-def passwd(username):
+def passwd(args):
+    username = args.username
     if os.path.isfile(AUTHDBPATH):
         print("Enter password for "+username+": ")
 
@@ -144,38 +148,46 @@ def passwd(username):
         conn.close()
     else:
         print("{}: Database file does not exist".format(sys.argv[0]),
-               file=sys.stderr)
+              file=sys.stderr)
 
 
-def main():
-    argc = len(sys.argv)
-    exitcode = 0
+def main(argv):
+    # create the top-level parser
+    parser = argparse.ArgumentParser()
+    # subparsers = parser.add_subparsers(help='Commands:')
+    subparsers = parser.add_subparsers()
 
-    if argc < 2:
-        usage()
-        exitcode = 1
-    else:
-        if argc < 3:
-            sys.argv.append(None)
+    parser_start = subparsers.add_parser('start', help='Start the server')
+    parser_start.add_argument(
+        '--configfile', '-c', type=str, help='config (.ini) file to use')
+    parser_start.set_defaults(func=startsrv, configfile="production.ini")
 
-        if sys.argv[1] == "start":
-            startsrv(sys.argv[2])
-        elif sys.argv[1] == "stop":
-            stopsrv()
-        elif sys.argv[1] == "adduser":
-            adduser(sys.argv[2])
-        elif sys.argv[1] == "deluser":
-            deluser(sys.argv[2])
-        elif sys.argv[1] == "lsuser":
-            lsuser()
-        elif sys.argv[1] == "passwd":
-            passwd(sys.argv[2])
-        else:
-            usage()
-            exitcode = 1
+    parser_stop = subparsers.add_parser('stop', help='Stop the server')
+    parser_stop.set_defaults(func=stopsrv)
 
-    sys.exit(exitcode)
+    parser_adduser = subparsers.add_parser(
+        'adduser', help='Add user <username>')
+    parser_adduser.add_argument('username', type=str)
+    parser_adduser.set_defaults(func=adduser)
+
+    parser_deluser = subparsers.add_parser(
+        'deluser', help='''Delete user <username> from the database''')
+    parser_deluser.add_argument('username', type=str)
+    parser_deluser.set_defaults(func=deluser)
+
+    parser_passwd = subparsers.add_parser(
+        'passwd', help='''Set the password for user <username>''')
+    parser_passwd.add_argument('username', type=str)
+    parser_passwd.set_defaults(func=passwd)
+
+    # create the parser for the "b" command
+    parser_lsuser = subparsers.add_parser(
+        'lsuser', help='List users')
+    parser_lsuser.set_defaults(func=lsuser)
+
+    args = parser.parse_args(argv)
+    args.func(args)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
